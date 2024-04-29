86b5ad1767e0facf7b95b7507aa29736a60d9694
Ian Griffin <griffinian870@gmail.com>
2024-04-28 21:24:33 -0400

    fixed pt 1 and 2

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const config = require('./utils/config')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const blogsRouter = require('./controllers/blogs')
-const middleware = require('./utils/middleware')
-const logger = require('./utils/logger')
-const mongoose = require('mongoose')
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const app = express();
+const blogsRouter = require('./controllers/blogs');
+const middleware = require('./utils/middleware');
+const logger = require('./utils/logger');
+const mongoose = require('mongoose');
 
-mongoose.set('strictQuery', false)
+mongoose.set('strictQuery', false);
 
-logger.info('connecting to', config.MONGODB_URI)
+logger.info('connecting to', config.MONGODB_URI);
 
 mongoose.connect(config.MONGODB_URI)
   .then(() => {
@@ -19,14 +21,15 @@ mongoose.connect(config.MONGODB_URI)
     logger.error('error connecting to MongoDB:', error.message)
   })
 
-app.use(cors())
-app.use(express.static('dist'))
-app.use(express.json())
-app.use(middleware.requestLogger)
+app.use(express.static('dist'));
+app.use(express.json());
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(middleware.requestLogger);
 
-app.use('/api/blogs', blogsRouter)
+app.use('/api/blogs', blogsRouter);
 
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
+// app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
 module.exports = app
--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const app = require('./app')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+// app.get('/api/blogs', (request, response) => {
+//   console.log('GET /api/blogs')
+//   response.json({ message: 'GET /api/blogs' })
+// });
+
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
 })
